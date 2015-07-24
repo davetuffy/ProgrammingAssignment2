7e05ca222ae40b350176bf94fa41d1c470c56699
David W. Tuffy <davetuffy@yahoo.com>
2015-07-24 17:49:09 -0400

    updating cachematrix with changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these functions cache the inverse of any invertable matrix
+## Once it is cached, the inverse can be retrurned for the same matrix, or a new matrix can be
+## re-supplied to be inverted and cached
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix (or creates a 0 lengthe on if none supplied) and defines the
+## functions that can be used to:
+## - set the Matrix value in a variable within the function environment
+## - get the Matrix from a variable within the function environment
+## - set the inverted matrix in a variable within the function environemnt
+## - get the inverted matrix from a variable within the function environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        iMat <- NULL
+        setMat <- function(y) {
+                x <<- y
+                iMat <<- NULL
+        }
+        getMat <- function() x
+        setinverted <- function(im) iMat <<- im
+        getinverted <- function() iMat
+        list(setMat = setMat, getMat = getMat,
+             setinverted = setinverted,
+             getinverted = getinverted)
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+        iMat <- x$getinverted()
+        if(!is.null(iMat)) {
+                message("getting cached inverted matrix")
+                return(iMat)
+        }
+        my_matrix <- x$getMat()
+        iMat <- solve(my_matrix)
+        x$setinverted(iMat)
+        iMat
 }
